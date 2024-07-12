4b484ede1af748c31bfd1eba8c02b7fcff6d61bf
anurag singh <140162738+anurag6569201@users.noreply.github.com>
2024-07-13 00:54:13 +0530

    updated data of sqlite

--- inoperate/models.py
+++ inoperate/models.py
@@ -3,4 +3,4 @@ from django.db import models
 class Person(models.Model):
     account_number = models.CharField(max_length=20)
     name = models.CharField(max_length=100)
-    amount=models.IntegerField()
+    amount=models.FloatField()
