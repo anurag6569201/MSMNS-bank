96a9199c1697d76803dfaa53680a4f66b2ef1f01
anurag singh <140162738+anurag6569201@users.noreply.github.com>
2024-07-13 20:40:14 +0530

    added deaf auths

--- inoperate/urls.py
+++ inoperate/urls.py
@@ -5,4 +5,9 @@ app_name="inoperate"
 
 urlpatterns=[
     path("",views.inoperate,name="inoperate"),
+    path("DEAF",views.view_full_deaf,name="view_full_deaf"),
+
+    # authentication urls
+    path('sign-in/',views.login_view,name='sign-in'),
+    path('sign-out/',views.logout_view,name='sign-out'),
 ]
--- inoperate/views.py
+++ inoperate/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from inoperate.forms import SearchForm
 from inoperate.models import Person
 from django.db.models import Sum
+
+from django.contrib.auth import login, authenticate
+from django.contrib import messages
+from django.conf import settings
+from django.contrib.auth import logout
+
+User=settings.AUTH_USER_MODEL
 
 def inoperate(request):
     no_of_account = Person.objects.all().count()
@@ -22,3 +29,37 @@ def inoperate(request):
         'total_amount':total_amount,
     }
     return render(request, "inoperate/app/inoperate.html", context)
+
+def view_full_deaf(request):
+    pass
+
+def login_view(request):
+    if request.user.is_authenticated:
+        messages.warning(request,f"Hey, You are already Logged in")
+        return redirect('inoperate:view_full_deaf')
+    else:
+        if request.method=="POST":
+            email=request.POST.get("email")
+            password=request.POST.get("password")
+
+            try:
+                user=User.object.get(email=email)
+            except:
+                messages.warning(request,f"User with {email} does not exist")
+
+            user=authenticate(request,email=email,password=password)
+
+            if user is not None:
+                login(request,user)
+                messages.success(request,"Your are logged in.")
+                return redirect("inoperate:view_full_deaf")
+            
+            else:
+                messages.warning(request,"User Does not exist, create an accounnt")
+    return render(request,"inoperate/app/sign-in.html")
+
+
+def logout_view(request):
+    logout(request)
+    messages.success(request, "You Logged-Out, successfully")
+    return redirect("inoperate:inoperate")
