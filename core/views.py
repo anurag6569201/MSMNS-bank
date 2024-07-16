0e2082d57f39dc927a5c9ba7bb9c9f45f461c681
anurag singh <140162738+anurag6569201@users.noreply.github.com>
2024-07-16 09:08:58 +0530

    added maintainance

--- core/views.py
+++ core/views.py
@@ -7,3 +7,9 @@ def index(request):
         'current_date': current_date
     }
     return render(request, "core/app/index.html", context)
+
+def underwork(request, data):
+    context = {
+        'data': data,
+    }
+    return render(request, "core/app/underwork.html", context)
